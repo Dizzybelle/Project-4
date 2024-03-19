bd0b7be0d99e745493a1b46f7b4458fc7fab17f7
bwfitzpa <137216561+bwfitzpa@users.noreply.github.com>
2024-03-18 20:34:02 -0400

    updating the second gauge chart

--- static/js/Project_4_app.js
+++ static/js/Project_4_app.js
@@ -1,10 +1,11 @@
 // Creating the lists to populate the dropdown menus
-const partyIDs = ["<select party>", "Democrat", "Republican", "Independent", "Other Party", "No Party Preference", "No Response"];
+const partyIDs = ["<select party>", "Democrat", "Republican", "Independent", "Other Party", "No Party Preference", 
+                "Don't Know", "No Response"];
 const ages = ["<select age>", 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 
               41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 
-              67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, "80+"];
-const genders = ["<select gender>", "Male", "Female", "Inapplicable", "No Response"];
-const locations = ["<select location>", "Rural", "Small Town", "Suburb", "City", "No Response"];
+              67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, "80+", "Don't Know", "No Response"];
+const genders = ["<select gender>", "Male", "Female", "Inapplicable", "Don't Know", "No Response"];
+const locations = ["<select location>", "Rural", "Small Town", "Suburb", "City", "Don't Know", "No Response"];
 const rightTrack = ["<select country track>", "Right", "Wrong", "Don't Know", "No Response"];
 const thermoBiden = ["<select Biden rating>", 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 
                     18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 
@@ -28,7 +29,7 @@ const income = ["<select income>", "Under $9,999", "$10,000-14,999", "$15,000-19
                 "$80,000-89,999", "$90,000-99,999", "$100,000-109,999", "$110,000-124,999", "$125,000-149,999",
                 "$150,000-174,999", "$175,000-249,999", "$250,000 or more", "Don't Know", "No Response"];
 const primaryVote = ["<select if voted in primary>" ,"Yes, voted in primary or caucus",
-                    " No, didn’t vote in primary or caucus", "Don't Know", "No Response"];
+                    "No, didn’t vote in primary or caucus", "Don't Know", "No Response"];
 // const agesVoting = ["<select age>"];
 // const gendersVoting = ["<select gender>"];
 // const raceEthnicityVoting = ["<select race/ethnicity>"];
@@ -241,16 +242,19 @@ function populateDropdowns() {
     });           
 }
 
-let selectedPartyID;
-let selectedAge;
-let mathTest;
-let partyNumber;
-let probVoteBiden;
-let probVoteTrump;
 
+
+//Section on predicting how likely someone is to vote for Biden or Trump//
+
+let selectedPartyID;
+let partyNumber;
+// Function to change partyID values
+function optionChangedParty(value) {
+    selectedPartyID = value;
+    optionPartyNumber();
+}
 // Function to convert partyID string to text and update the party number
-/////////////////////// Need to update this to instead give the coeficient value
-/////////////////////// of each dummy
+//***Need to update this to instead give the coeficient value
 function optionPartyNumber() {
     if (selectedPartyID === "Democrat") {
         partyNumber = 1;
@@ -263,37 +267,200 @@ function optionPartyNumber() {
     presidentVoteProb();
 }
 
-// Function to change partyID dropdown menu
-function optionChangedParty(value) {
-    selectedPartyID = value;
-    optionPartyNumber();
-}
-
-// Function to change age dropdown menu
+let selectedAge;
+// Function to change age dropdown values
 function optionChangedAge(value) {
     selectedAge = parseInt(value);
     //Update presidentVoteProb
     presidentVoteProb();
 }
 
-////////// Need to create a  function to convert gender to coefficient values 
-////////// from each dummy variable
-// Function to change gender dropdown menu
+let selectedGender;
+let genderNumber
+// Function to change gender dropdown values
 function optionChangedGender(value) {
     selectedGender = value;
+    //Update presidentVoteProb
+    optionGenderNumber();
+}
+//***Function to convert gender to coefficient values from each dummy variable, using 1 & 2 as placeholders now
+function optionGenderNumber() {
+    if (selectedGender === "Male") {
+        genderNumber = 1;
+    } else if (selectedGender === "Female") {
+        genderNumber = 2;
+    } else {
+        genderNumber = 0;
+    }
+    //Update presidentVoteProb
+    presidentVoteProb();
 }
 
+let selectedLocation;
+let locationNumber
+// Function to change location dropdown values
+function optionChangedLocation(value) {
+    selectedLocation = value;
+    //Update presidentVoteProb
+    optionLocationNumber();
+}
+//***Function to convert location to coefficient values from each dummy variable, using 1 & 2 as placeholders now
+function optionLocationNumber() {
+    if (selectedLocation === "Rural") {
+        locationNumber = 1;
+    } else if (selectedLocation === "Small Town") {
+        locationNumber = 2;
+    } else if (selectedLocation === "Suburb") {
+        locationNumber = 3;
+    } else if (selectedLocation === "City") {
+        locationNumber = 4;
+    } else {
+        locationNumber = 0;
+    }
+    //Update presidentVoteProb
+    presidentVoteProb();
+}
 
+let selectedRightTrack;
+let rightTrackNumber
+// Function to change right track dropdown values
+function optionChangedRightTrack(value) {
+    selectedRightTrack = value;
+    //Update presidentVoteProb
+    optionRightTrackNumber();
+}
+//***Function to convert right track to coefficient values from each dummy variable, using 1 & 2 as placeholders now
+function optionRightTrackNumber() {
+    if (selectedRightTrack === "Right") {
+        rightTrackNumber = 1;
+    } else if (selectedRightTrack === "Wrong") {
+        rightTrackNumber = 2;
+    } else {
+        rightTrackNumber = 0;
+    }
+    //Update presidentVoteProb
+    presidentVoteProb();
+}
 
+let selectedThermoBiden;
+// Function to change ThermoBiden dropdown values
+function optionChangedThermoBiden(value) {
+    selectedThermoBiden = parseInt(value);
+    //Update presidentVoteProb
+    presidentVoteProb();
+}
+
+let selectedThermoTrump;
+// Function to change ThermoTrump dropdown values
+function optionChangedThermoTrump(value) {
+    selectedThermoTrump = parseInt(value);
+    //Update presidentVoteProb
+    presidentVoteProb();
+}
+
+let selectedRaceEthnicity;
+let raceEthnicityNumber
+// Function to change right track dropdown values
+function optionChangedRaceEthnicity(value) {
+    selectedRaceEthnicity = value;
+    //Update presidentVoteProb
+    optionRaceEthnicityNumber();
+}
+//***Function to convert right track to coefficient values from each dummy variable, using 1 & 2 as placeholders now
+function optionRaceEthnicityNumber() {
+    if (selectedRaceEthnicity === "White, non-Hispanic") {
+        raceEthnicityNumber = 1;
+    } else if (selectedRaceEthnicity === "Black, non-Hispanic") {
+        raceEthnicityNumber = 2;
+    } else if (selectedRaceEthnicity === "Hispanic") {
+        raceEthnicityNumber = 3;
+    } else if (selectedRaceEthnicity === "Asian or Native Hawaiian/other Pacific Islander, non-Hispanic alone") {
+        raceEthnicityNumber = 4;
+    } else if (selectedRaceEthnicity === "Native American/Alaska Native or other race, non-Hispanic alone") {
+        raceEthnicityNumber = 5;
+    } else if (selectedRaceEthnicity === "Multiple races, non-Hispanic") {
+        raceEthnicityNumber = 6;
+    } else {
+        raceEthnicityNumber = 0;
+    }
+    //Update presidentVoteProb
+    presidentVoteProb();
+}
+
+let selectedIncome;
+let incomeNumber
+// Function to change RaceEthnicity dropdown values
+function optionChangedIncome(value) {
+    selectedIncome = value;
+    //Update presidentVoteProb
+    optionIncomeNumber();
+}
+//***Function to convert RaceEthnicity to coefficient values from each dummy variable, using 1 & 2 as placeholders now
+function optionIncomeNumber() {
+    if (selectedIncome === "Under $9,999") {
+        incomeNumber = 1;
+    } else if (selectedIncome === "$10,000-14,999") {
+        incomeNumber = 2;
+    } else if (selectedIncome === "$15,000-19,999") {
+        incomeNumber = 3;
+    } else if (selectedIncome === "$20,000-24,999") {
+        incomeNumber = 4;
+    } else if (selectedIncome === "$25,000-29,999") {
+        incomeNumber = 5;
+    } else if (selectedIncome === "$30,000-34,999") {
+        incomeNumber = 6;
+    } else if (selectedIncome === "$35,000-39,999") {
+        incomeNumber = 7;
+    } else if (selectedIncome === "$40,000-44,999") {
+        incomeNumber = 8;
+    } else if (selectedIncome === "$45,000-49,999") {
+        incomeNumber = 9;
+    } else if (selectedIncome === "$50,000-59,999") {
+        incomeNumber = 10;
+    } else if (selectedIncome === "$60,000-64,999") {
+        incomeNumber = 11;
+    } else if (selectedIncome === "$65,000-69,999") {
+        incomeNumber = 12;
+    } else if (selectedIncome === "$70,000-74,999") {
+        incomeNumber = 13;
+    } else if (selectedIncome === "$75,000-79,999") {
+        incomeNumber = 14;
+    } else if (selectedIncome === "$80,000-89,999") {
+        incomeNumber = 15;
+    } else if (selectedIncome === "$90,000-99,999") {
+        incomeNumber = 16;
+    } else if (selectedIncome === "$100,000-109,999") {
+        incomeNumber = 17;
+    } else if (selectedIncome === "$110,000-124,999") {
+        incomeNumber = 18;
+    } else if (selectedIncome === "$125,000-149,999") {
+        incomeNumber = 19;
+    } else if (selectedIncome === "$150,000-174,999") {
+        incomeNumber = 20;
+    } else if (selectedIncome === "$175,000-249,999") {
+        incomeNumber = 21;
+    } else if (selectedIncome === "$250,000 or more") {
+        incomeNumber = 22;
+    } else {
+        incomeNumber = 0;
+    }
+    //Update presidentVoteProb
+    presidentVoteProb();
+}
+
+let probVoteBiden;
+let probVoteTrump;
 // Function to calculate probability of voting for Biden or Trump
 function presidentVoteProb() {
     // Biden vote probability
-    //NEED TO REPLACE WITH ACTUAL FORMULA JUST TESTING FOR NOW
-        probVoteBiden = (partyNumber/2) * (selectedAge/80);
+    //****NEED TO REPLACE WITH ACTUAL FORMULA JUST TESTING FOR NOW
+        probVoteBiden = (partyNumber) * (selectedAge/80) * (genderNumber) * (locationNumber) * 
+        (rightTrackNumber) * (selectedThermoBiden/50) * (selectedThermoTrump/50) * (raceEthnicityNumber/6) * 
+        (incomeNumber/21);
         probVoteTrump = 1 - probVoteBiden; 
         // Update probVoteBiden
-        document.getElementById("probVoteBiden").innerHTML = probVoteBiden;
-        document.getElementById("probVoteTrump").innerHTML = probVoteTrump;
+        document.getElementById("probVoteBiden").innerHTML = probVoteBiden.toFixed(5);
+        document.getElementById("probVoteTrump").innerHTML = probVoteTrump.toFixed(5);
         gaugeChart1();
 }
 
@@ -364,12 +531,91 @@ populateDropdowns();
 
 
 
+//Section on the likelihood of someone voting in the general election
+let selectedPrimary;
+let primaryNumber
+// Function to change right track dropdown values
+function optionChangedPrimary(value) {
+    selectedPrimary = value;
+    //Update presidentVoteProb
+    optionPrimaryNumber();
+}
+//***Function to convert right track to coefficient values from each dummy variable, using 1 & 2 as placeholders now
+function optionPrimaryNumber() {
+    if (selectedPrimary === "Yes, voted in primary or caucus") {
+        primaryNumber = 1;
+    } else if (selectedPrimary === "No, didn’t vote in primary or caucus") {
+        primaryNumber = 2;
+    } else {
+        primaryNumber = 0;
+    }
+    //Update presidentVoteProb
+    votingProb();
+}
+    
+////////////////////////////////////////////////////////////////////////////////////////////////////
+            
+//     // agesVoting dropdown
+//     ages.forEach(ageVoting => {
+//         const option = document.createElement("option");
+//         option.text = ageVoting;
+//         option.value = ageVoting;
+//         agesVotingSelect.appendChild(option);
+//     });  
+//     // gendersVoting dropdown
+//     genders.forEach(gender => {
+//         const option = document.createElement("option");
+//         option.text = gender;
+//         option.value = gender;
+//         gendersVotingSelect.appendChild(option);
+//     });              
+//     //raceEthnicityVoting dropdown
+//     raceEthnicity.forEach(race => {
+//         const option = document.createElement("option");
+//         option.text = race;
+//         option.value = race;
+//         raceEthnicityVotingSelect.appendChild(option);
+//     });             
+//     //yearsCommunity dropdown
+//     yearsCommunity.forEach(years => {
+//         const option = document.createElement("option");
+//         option.text = years;
+//         option.value = years;
+//         yearsCommunitySelect.appendChild(option);
+//     });        
+//     //incomeVoting dropdown
+//     income.forEach(incomes => {
+//         const option = document.createElement("option");
+//         option.text = incomes;
+//         option.value = incomes;
+//         incomeVoingSelect.appendChild(option);
+//     });          
+//     //swingState dropdown
+//     swingState.forEach(swing => {
+//         const option = document.createElement("option");
+//         option.text = swing;
+//         option.value = swing;
+//         swingStateSelect.appendChild(option);
+//     });
+////////////////////////////////////////////////////////////////////////////////////////////////////
+
+let probVoting;
+// Function to calculate probability of voting for Biden or Trump
+function votingProb() {
+    // Biden vote probability
+    //****NEED TO REPLACE WITH ACTUAL FORMULA JUST TESTING FOR NOW
+        probVoting = (primaryNumber/2);
+        // Update probVoteBiden
+        document.getElementById("probVoting").innerHTML = probVoting.toFixed(5);
+        gaugeChart2();
+}
+votingProb()
 //****NEED TO CHANGE VALUE TO PROBABILITY OF VOTING//////
 //Creating the gauge chart for probability of voting
 function gaugeChart2() {
     let data = [{
         domain: { x: [0, 1], y: [0, 1] },
-        value: 0.45,
+        value: probVoting,
         type: "indicator",
         mode: "gauge+number",
         gauge: {
@@ -421,6 +667,52 @@ gaugeChart2()
 
 
 
+////////////////////////////////////////////////////////////////////////////////////////////////////
+//     //ideology dropdown
+//     ideology.forEach(ideologies => {
+//         const option = document.createElement("option");
+//         option.text = ideologies;
+//         option.value = ideologies;
+//         ideologySelect.appendChild(option);
+//     });            
+//     //partyStrenght dropdown
+//     partyStrength.forEach(parties => {
+//         const option = document.createElement("option");
+//         option.text = parties;
+//         option.value = parties;
+//         partyStrenghtSelect.appendChild(option);
+//     });                  
+//     //dislikeBiden dropdown
+//     dislikeBiden.forEach(bidenDislike => {
+//         const option = document.createElement("option");
+//         option.text = bidenDislike;
+//         option.value = bidenDislike;
+//         dislikeBidenSelect.appendChild(option);
+//     });        
+//     //dislikeTrump dropdown
+//     dislikeTrump.forEach(trumpDislike => {
+//         const option = document.createElement("option");
+//         option.text = trumpDislike;
+//         option.value = trumpDislike;
+//         dislikeTrumpSelect.appendChild(option);
+//     });           
+//     //splitUnified dropdown
+//     splitUnified.forEach(unifiedSplit => {
+//         const option = document.createElement("option");
+//         option.text = unifiedSplit;
+//         option.value = unifiedSplit;
+//         splitUnifiedSelect.appendChild(option);
+//     });          
+//     //countryTrack dropdown
+//     countryTrack.forEach(trackCountry => {
+//         const option = document.createElement("option");
+//         option.text = trackCountry;
+//         option.value = trackCountry;
+//         countryTrackSelect.appendChild(option);
+//     });           
+// }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 //****NEED TO CHANGE VALUE TO PROBABILITY OF being a swing voter//////
 //Creating the gauge chart for probability of being a swing voter
